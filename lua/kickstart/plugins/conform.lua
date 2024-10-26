412ed83a8783750080f35e56df6f0bd103da0a30
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-26 14:46:22 -0500

    feat: add support for multiple file types in conform configuration

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -37,7 +37,13 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
