3c99d3d1928503448d4040f64927bd7dff9ab4d2
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-26 17:27:50 -0500

    feat: add incline.nvim plugin with custom configuration for filename display

--- lua/kickstart/plugins/ui.lua
+++ lua/kickstart/plugins/ui.lua
@@ -48,4 +48,35 @@ return {
       }
     end
   },
+
+  -- filename
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("kanagawa.colors").setup({ theme = "dragon" })
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.palette.sumiInk0, guifg = colors.palette.dragonRed },
+            InclineNormalNC = { guifg = colors.palette.sumiInk0, guibg = colors.palette.dragonRed },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
 }
