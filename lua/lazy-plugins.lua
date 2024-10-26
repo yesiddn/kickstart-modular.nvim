aa1da139fb2b1461f9bbf49b669344da57fb6887
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-26 18:41:19 -0500

    feat: add copilot.lua plugin to lazy loading configuration

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -60,6 +60,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.ui',
+  require 'kickstart.plugins.copilot',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
