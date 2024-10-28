e7d24001377b743b651f51aaada45712ef3e463a
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-27 19:00:31 -0500

    feat: add key mappings for buffer and git commands in which-key

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -64,6 +64,8 @@ return {
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>b', group = '[B]uffer' },
+        { '<leader>g', group = '[G]it' },
       },
     },
   },
