e51a3ff3b7e9efbeebf9d8feecf1cf2d5fae9034
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-27 21:31:30 -0500

    feat: add root directory detection configuration

--- lua/options.lua
+++ lua/options.lua
@@ -62,4 +62,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+
 -- vim: ts=2 sts=2 sw=2 et
