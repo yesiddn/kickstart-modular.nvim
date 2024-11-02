c0b3b75ebb0d2fdb414bee8393244d0c21f3659b
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-01 23:49:18 -0500

    feat: enhance rainbow-delimiters plugin configuration with custom highlight colors

--- lua/custom/plugins/rainbow-delimiters.lua
+++ lua/custom/plugins/rainbow-delimiters.lua
@@ -1,5 +1,18 @@
 return {
   {
-    "HiPhish/rainbow-delimiters.nvim"
+    'HiPhish/rainbow-delimiters.nvim',
+    config = function()
+      require('rainbow-delimiters.setup').setup {
+        highlight = {
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterRed',
+        },
+      }
+    end,
   }
 }
