4e29e5000588b65a3917394431408c9eb8add1f6
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-03 17:15:35 -0500

    feat: update dashboard-nvim configuration to improve loading and add new buttons

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,14 +1,33 @@
 return {
+  -- dashboard
   {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        theme = 'doom',
+    "nvimdev/dashboard-nvim",
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    opts = function()
+      -- local logo = [[
+      --      ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+      --      ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+      --      ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+      --      ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+      --      ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+      --      ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+      -- ]]
+
+      -- logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
         config = {
           week_header = {
             enable = true,
           },
+          -- header = vim.split(logo, "\n"),
+          -- stylua: ignore
           center = {
             {
               icon = ' ',
@@ -42,11 +61,51 @@ return {
               action = 'Telescope oldfiles',
               key = 'h',
             },
+            {
+              icon = ' ',
+              icon_hl = '@function',
+              desc = 'Lazy',
+              group = 'Label',
+              action = 'Lazy',
+              key = 'l',
+            },
+            {
+              icon = ' ',
+              icon_hl = '@error',
+              desc = 'Close All',
+              group = 'Label',
+              action = 'qa',
+              key = 'q',
+            },
           },
-          footer = {}
-        }
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
       }
-    end
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- open dashboard after closing lazy
+      if vim.o.filetype == "lazy" then
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+            end)
+          end,
+        })
+      end
+
+      return opts
+    end,
   },
 
   -- filename
