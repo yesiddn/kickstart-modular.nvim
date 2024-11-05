d497bd4255c0a0aaa0a85ab1a79e079b6a5b9735
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-04 21:31:39 -0500

    feat: add TSX support and HTML autocommands in treesitter configuration

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
       ensure_installed = { 
         'javascript',
         'typescript',
+        'tsx',
         'json',
         'html',
         'css', 
@@ -32,7 +33,19 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      autotag = { enable = true },
     },
+    config = function()
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+        pattern = { "*.component.html", "*.container.html" },
+        callback = function()
+          vim.treesitter.start(nil, "html") -- Usa el parser de HTML o "angular" si tienes uno específico
+        end,
+      })
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
